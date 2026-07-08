0d858d771c1848ca76bbe99e45f1a6c1071a5e6e
JubeleeDev <jubeleedev@yandex.ru>
2026-07-08 17:13:17 +0700

    wrote my first test for generator

--- internal/shortener/generator_test.go
+++ internal/shortener/generator_test.go
@@ -1,1 +1,32 @@
 package shortener
+
+import "testing"
+
+func TestGenerateCodeReturnsRequestedLength(t *testing.T) {
+	code, err := GenerateCode(5)
+
+	if err != nil {
+		t.Fatalf("expected err == nil, got %v", err)
+	}
+
+	if len(code) != 5 {
+		t.Errorf("expected code length = 5, got %d", len(code))
+	}
+
+}
+
+func TestGenerateCodeReturnsErrorForZeroLength(t *testing.T) {
+	_, err := GenerateCode(0)
+
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestGenerateCodeReturnsErrorForNegativeLength(t *testing.T) {
+	_, err := GenerateCode(-3)
+
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
